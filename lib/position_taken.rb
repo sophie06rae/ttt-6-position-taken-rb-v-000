ab92e69bb8d67c1aed9a2f1ef5477788a4892c8a
Sophie Rae <sophie.rae.06@gmail.com>
2017-09-14 08:22:28 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
- board[index] != "" || board[index]!= " "
+ if board[index] != ""
+ else
+ board[index]!= " "
+ end
 end
